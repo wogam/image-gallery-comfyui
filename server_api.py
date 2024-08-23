7c67eb249807bd5b18fe9173afee188912cc0480
wogam <48340127+wogam@users.noreply.github.com>
2024-08-23 20:50:17 +0100

    Update server_api.py
    Add support for moving images

--- server_api.py
+++ server_api.py
@@ -1,5 +1,6 @@
 import os
 import json
+import shutil
 import folder_paths
 from server import PromptServer
 from aiohttp import web
@@ -89,3 +90,67 @@ async def get_gallery_images(request):
     return web.Response(text=json.dumps(response_data), content_type='application/json')
 
 PromptServer.instance.routes.get('/gallery/images')(get_gallery_images)
+
+async def move_folder(request):
+    data = await request.post()
+
+    dir_type = data.get('type', 'output')
+    dir_path = folder_paths.get_directory_by_type(dir_type)
+    if dir_path is None:
+        return web.Response(status=400)
+
+    if 'subfolder' in data:
+        dir_path = os.path.join(dir_path, data['subfolder'])
+
+    foldername = data.get('foldername')
+    destination = data.get('destination')
+    if foldername is None or destination is None:
+        return web.Response(status=400)
+
+    source_path = os.path.normpath(os.path.join(dir_path, foldername))
+    dest_path = os.path.normpath(os.path.join(dir_path, destination, foldername))
+
+    if os.path.commonpath([source_path, dir_path]) != os.path.commonpath([dir_path]) or \
+       os.path.commonpath([dest_path, dir_path]) != os.path.commonpath([dir_path]):
+        return web.Response(status=403)
+
+    if not os.path.isdir(source_path):
+        return web.Response(status=404)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    shutil.move(source_path, dest_path)
+    return web.Response(status=204)
+
+PromptServer.instance.routes.post('/gallery/folder/move')(move_folder)
+
+async def move_image(request):
+    data = await request.post()
+
+    dir_type = data.get('type', 'output')
+    dir_path = folder_paths.get_directory_by_type(dir_type)
+    if dir_path is None:
+        return web.Response(status=400)
+
+    if 'subfolder' in data:
+        dir_path = os.path.join(dir_path, data['subfolder'])
+
+    filename = data.get('filename')
+    destination = data.get('destination')
+    if filename is None or destination is None:
+        return web.Response(status=400)
+
+    source_path = os.path.normpath(os.path.join(dir_path, filename))
+    dest_path = os.path.normpath(os.path.join(dir_path, destination, filename))
+
+    if os.path.commonpath([source_path, dir_path]) != os.path.commonpath([dir_path]) or \
+       os.path.commonpath([dest_path, dir_path]) != os.path.commonpath([dir_path]):
+        return web.Response(status=403)
+
+    if not os.path.isfile(source_path):
+        return web.Response(status=404)
+
+    os.makedirs(os.path.dirname(dest_path), exist_ok=True)
+    shutil.move(source_path, dest_path)
+    return web.Response(status=204)
+
+PromptServer.instance.routes.post('/gallery/image/move')(move_image)
